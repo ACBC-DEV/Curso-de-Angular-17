79e2f27f064fa1aede293f2689cee1e2e82798e1
acbc <acbc.dev@gmail.com>
2024-04-23 17:36:00 -0500

    Fix imports and update properties in labs.component.ts

--- todoapp/src/app/pages/labs/labs.component.ts
+++ todoapp/src/app/pages/labs/labs.component.ts
@@ -1,24 +1,40 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf],
   templateUrl: './labs.component.html',
   styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   welcom = 'Welcome todoApp';
-  name = 'RAUL';
-  tasks = ['First task', 'Second task', 'Third task'];
+  name = signal('ACBC');
+  tasks = signal(['First task', 'Second task', 'Third task']);
 
   disable = true;
 
   img = 'https://angular.io/assets/images/logos/angular/angular.png';
-  person = {
+  person = signal({
     name: 'Raul',
-    age: 22,
+    age: 10,
     job: 'Developer',
-  };
+  });
+
+  clickHandler() {
+    alert('Button clicked!');
+  }
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.name.set(newValue);
+  }
+  keydownHandler(event: KeyboardEvent) {
+    const input = event.target as HTMLInputElement;
+    console.log('Key down!', input.value);
+  }
+  changeAge() {
+    this.person.update((prev) => ({ ...prev, age: prev.age + 1 }));
+  }
 }
